127cb7c9568635df09f3c191297fc206a10a508d
agatpan <Krokodyl83>
2017-12-21 21:12:29 +0100

    name change

--- HalloWorld.java
+++ HalloWorld.java
@@ -3,6 +3,6 @@ package com.infoshareacademy;
 public class HalloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World");
+        System.out.println("Hello, Agata");
     }
 }
